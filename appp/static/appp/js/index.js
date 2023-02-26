f91afd0f619128290a8457d2bebbbe21befc0b5b
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-26 17:48:16 +0530

    making forms,along with adding statis folder

---
+++ appp/static/appp/js/index.js
@@ -0,0 +1,6 @@
+const menuIcon = document.querySelector('.menu-icon');
+const menuItems = document.querySelector('.menu-items');
+
+menuIcon.addEventListener('click', () => {
+  menuItems.classList.toggle('show');
+});

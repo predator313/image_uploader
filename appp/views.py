867516aa529ef5d087be221248c836da13db806e
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-26 17:04:22 +0530

    making class based view

--- appp/views.py
+++ appp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+# def home(request):
+#     return render(request,'appp/home.html')
+
+class Home(TemplateView):
+    template_name='appp/home.html'

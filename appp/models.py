d7780357e32dd5e788a571cb754d761b3f1c8361
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-26 15:16:48 +0530

    image model is created

--- appp/models.py
+++ appp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    photo=models.ImageField(upload_to="myimg")
+    date=models.DateTimeField(auto_now_add=True)
+    
+
